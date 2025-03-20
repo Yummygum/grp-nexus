84ee017a90edbc1e2a18c0d7f55b000f8ffadf91
donovanroubos <donovan@roubos.co>
2025-03-20 14:03:37 +0100

    chore: Initial setup

---
+++ packages/vue/src/components/Button.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface Props {
+  msg?: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <button class="sjakie">
+    <slot /> {{msg}}
+  </button>
+</template>
